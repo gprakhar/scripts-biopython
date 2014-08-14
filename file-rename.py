e2d114897195fc7866b0aff9387866a11ed5bdf4
EC2 Default User <ec2-user@ip-172-31-15-183.ec2.internal>
2014-08-14 13:47:32 +0000

    first commit

---
+++ file-rename.py
@@ -0,0 +1,16 @@
+cript to rename file according to particular pattern
+##Author : Prakhar Gaur
+##Licence GNU GPL3
+##Date Thu Aug 14 13:21:21 UTC 2014
+# 26_S27_L001_R2_001.fastq
+
+
+import os, re
+
+badprefix = "cheese_"
+#fnames = listdir('.')
+
+for fname in os.listdir("."):
+#    if fname.startswith(badprefix*2):
+     fname = re.sub
+        rename(fname, fname.replace(badprefix, '', 1))
