7fe8263e3112ca90f4fd900924458c8d4a4f7c04
EC2 Default User <ec2-user@ip-172-31-15-183.ec2.internal>
2014-08-27 13:29:53 +0000

    file-rename.py for renaming fastq file using regex is working now

--- file-rename.py
+++ file-rename.py
@@ -1,15 +1,27 @@
-cript to rename file according to particular pattern
+##Script to rename file according to particular pattern
 ##Author : Prakhar Gaur
 ##Date Thu Aug 14 13:21:21 UTC 2014
 # 26_S27_L001_R2_001.fastq
-
+# 29_S2_L001_R1_001.fastq
 
 import os, re
 
-badprefix = "cheese_"
-#fnames = listdir('.')
+regex_fname = r"[0-9]{1,2}_S[0-9]{1,2}_L001_R[12]{1,1}_001\.fastq"
+regex_replace_fr = r"_S[0-9]{1,2}_L001"
+regex_replace_bw = r"_001"
+#regex = re.compile(regex_fname)
+#regex = re.compile(regex_replace)
+
+#os.chdir("/data/raw_reads/")
+
 
 for fname in os.listdir("."):
-#    if fname.startswith(badprefix*2):
-     fname = re.sub
-        rename(fname, fname.replace(badprefix, '', 1))
+#    ans = regex.search(fname)
+#    print ans.group()
+    ans_1 = re.sub(regex_replace_fr, "", fname, 1)
+#    print ans_1
+    ans_2 = re.sub(regex_replace_bw, "", ans_1, 1)
+    print ans_2
+    os.rename(fname, ans_2)
+
+    
