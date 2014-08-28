ce8a4eb7d91fffe327c57480df0ab98d64e899ce
EC2 Default User <ec2-user@ip-172-31-15-183.ec2.internal>
2014-08-28 10:22:55 +0000

    run_command_loop.py is working now, had syntax mistake in if conditional

--- run_command_loop.py
+++ run_command_loop.py
@@ -4,13 +4,13 @@
 
 import os;
 for i in range(1,40):
-    if (i!=24 or i!=25):
-      cmdstr = '/bin/echo {0}'.format(i)
-      cmdPANDA = '/home/ec2-user/local_bin/PANDAseq/INSTALL/bin/pandaseq -f /data/raw_reads/{0}_R1.fastq  -r /data/raw_reads/{0}_R2.fastq -p CCTACGGGAGGCAGCAG -q ATTACCGCGGCTGCTGG -g {0}_PE-Read-assembly.log -T 8 -w {0}_out_aligned.fasta -U {0}_un-aligned.txt'.format(i) 
-      os.system(cmdstr)
+    if i not in (24,25):      
+        cmdstr = '/bin/echo {0}'.format(i)
+        cmdPANDA = '/home/ec2-user/local_bin/PANDAseq/INSTALL/bin/pandaseq -f /data/raw_reads/{0}_R1.fastq  -r /data/raw_reads/{0}_R2.fastq -p CCTACGGGAGGCAGCAG -q ATTACCGCGGCTGCTGG -g {0}_PE-Read-assembly.log -T 8 -w {0}_out_aligned.fasta -U {0}_un-aligned.txt'.format(i) 
+        os.system(cmdstr)
+        os.system(cmdPANDA)
     else:
       print NA
-#    os.system(cmdPANDA)
 
 
 
