60471fb31fa5950686ddc4254137354f0e933a75
gprakhar <prakhar.aaidu16@gmail.com>
2015-07-08 18:18:23 +0530

    run_command_loop.pycode clean up

--- run_command_loop.py
+++ run_command_loop.py
@@ -1,14 +1,13 @@
-##Script to run PANDAseq to merge reads iteratively in all 39 samples
-##incoming = 'arbit'
-#result = '{0} hello world {0} hello world {0}'.format(incoming)
+##Script to run PANDAseq to iteratively do something
+#Python 3
+#Author : prakhar gaur
+#Date : Sep 4 IST 2014
 
 import os;
 for i in range(1,40):
     if i not in (24,25):      
         cmdstr = '/bin/echo {0}'.format(i)
-        cmdPANDA = '/home/ec2-user/local_bin/PANDAseq/INSTALL/bin/pandaseq -f /data/raw_reads/{0}_R1.fastq  -r /data/raw_reads/{0}_R2.fastq -p CCTACGGGAGGCAGCAG -q ATTACCGCGGCTGCTGG -g {0}_PE-Read-assembly.log -T 8 -w {0}_out_aligned.fasta -U {0}_un-aligned.txt'.format(i) 
         os.system(cmdstr)
-        os.system(cmdPANDA)
     else:
       print 'NA'
 
