f784f271c79c292c5b7c699f3a55a09e641f87e1
gprakhar <prakhar.aaidu16@gmail.com>
2015-07-14 00:06:20 +0530

    script to do translation of CDS, can do six frame too

--- biopython-scripts/six-frame-translator.py
+++ biopython-scripts/six-frame-translator.py
@@ -1,22 +1,26 @@
-#Script to translate fasta sequence to six reading frames
+#Script to translate CDS sequence to corresponding protei seq
 #author: Prakhar gaur
 #date: 13 July 2015
 
 from Bio import SeqIO
 from Bio.SeqUtils import six_frame_translations
 
+import argparse
 
-for seq_record in SeqIO.parse("test.fa", "fasta"):
-	print(seq_record.id)
-#	print(seq_record.seq)
-#	print(len(seq_record))
-	print(six_frame_translations(seq_record.seq))
+parser = argparse.ArgumentParser()
+parser.add_argument('nameofInputfile', metavar='I', help='Name of fasta input file with CDS sequences')
 
+args = parser.parse_args()
 
-'''
-print(record.seq.translate(to_stop=True))
-print("Offset by one")
-print(record.seq[1:].translate(to_stop=True))
-print("Offset by two")
-print(record.seq[2:].translate(to_stop=True))
-'''
+filename = args.nameofInputfile
+
+for seq_record in SeqIO.parse(filename, 'fasta'):
+	print(seq_record.id) #Print Seq Id
+	print(seq_record.seq) # print the seq itself
+	print(len(seq_record)) # print lenght of seq
+	print('Translation: First Frame')
+	print(seq_record.seq.translate(to_stop=True)) #Translate the sequence
+#	print('Translation: Second Frame')
+#	print(seq_record.seq[1:].translate(to_stop=True))
+#	print('Translation: Third Frame')
+#	print(seq_record.seq[2:].translate(to_stop=True))
