0ff71deab369ffa133db52912541ba6628f685e2
gprakhar <prakhar.aaidu16@gmail.com>
2015-07-14 05:12:27 +0530

    blastXML-parser.py modified to produce output required for Monte carlo blast pipeline

--- biopython-scripts/blastXML-parser.py
+++ biopython-scripts/blastXML-parser.py
@@ -1,5 +1,6 @@
 #blast xml output parser using BioPython
-
+#author: prakhar gaur
+#date: 14 July 2015
 
 from Bio.Blast import NCBIXML
 
@@ -9,19 +10,25 @@ result_handle = open("disco-out.xml")
 #read xml output file
 blast_records = NCBIXML.parse(result_handle)
 
-
+'''
 #evalue cutoff
 E_VALUE_THRESH = 0.001
 
 for blast_record in blast_records:
 	for alignment in blast_record.alignments:
 		for hsp in alignment.hsps:
-			if hsp.expect < E_VALUE_THRESH:
-				print('****Alignment****')
+#			if hsp.expect < E_VALUE_THRESH:
+#				print('****Alignment****')
 				print('sequence:', alignment.title)
 				print('length:', alignment.length)
 				print('e value:', hsp.expect)
-				print(hsp.query[0:75] + '...')
-				print(hsp.match[0:75] + '...')
-				print(hsp.sbjct[0:75] + '...')
+#				print(hsp.query[0:75] + '...')
+#				print(hsp.match[0:75] + '...')
+#				print(hsp.sbjct[0:75] + '...')
+'''
+
+for blast_record in blast_records:
+	print blast_record.query #prints query's full name
+	for description in blast_record.descriptions:
+		print description.e # loops on all hits for the query and prints the corresponding e-value
 
