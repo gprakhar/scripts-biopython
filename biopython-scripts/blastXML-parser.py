de262b3405f7461ce788f6c3081a95b4490d97b9
gprakhar <prakhar.aaidu16@gmail.com>
2015-07-16 17:59:35 +0530

    blastXML-parser.py ready

--- biopython-scripts/blastXML-parser.py
+++ biopython-scripts/blastXML-parser.py
@@ -3,32 +3,32 @@
 #date: 14 July 2015
 
 from Bio.Blast import NCBIXML
-
-#open the blast xml output file
-result_handle = open("disco-out.xml")
-
-#read xml output file
-blast_records = NCBIXML.parse(result_handle)
-
+import os, glob
 '''
-#evalue cutoff
-E_VALUE_THRESH = 0.001
+import argparse
 
-for blast_record in blast_records:
-	for alignment in blast_record.alignments:
-		for hsp in alignment.hsps:
-#			if hsp.expect < E_VALUE_THRESH:
-#				print('****Alignment****')
-				print('sequence:', alignment.title)
-				print('length:', alignment.length)
-				print('e value:', hsp.expect)
-#				print(hsp.query[0:75] + '...')
-#				print(hsp.match[0:75] + '...')
-#				print(hsp.sbjct[0:75] + '...')
+parser = argparse.ArgumentParser()
+parser.add_argument('lenghtofAminoAcidSe', metavar='N', help='lenght of the amino acid sequence', type=int)
+parser.add_argument('-v',
 '''
 
-for blast_record in blast_records:
-	print blast_record.query #prints query's full name
-	for description in blast_record.descriptions:
-		print description.e # loops on all hits for the query and prints the corresponding e-value
+filenames = [os.path.basename(x) for x in glob.glob('*.xml')]
 
+e_value_dict = {}
+
+for name in filenames:
+	with open(name, 'r') as result_handle:
+		#read xml output file
+		blast_records = NCBIXML.parse(result_handle)
+		for blast_record in blast_records:
+			#print (blast_record.query) #prints query's full name
+			for description in blast_record.descriptions:
+				#print description.e # loops on all hits for the query and prints the corresponding e-value
+				#e_value_dict[blast_record.query] = description.e
+				print "%s : %s" % (blast_record.query, str(description.e))
+
+'''			
+with open("out1", "w") as out1, open("out2", "w") as out2:
+        for (k,v) in e_value_dict.items():
+                out1.write("%s\n" % k)
+                out2.write("%s\n" % str(v))'''
