029c91d89f91678106dc1bbfa7c0c2282ce7fb08
gprakhar <prakhar.aaidu16@gmail.com>
2015-07-25 00:14:56 +0530

    more modifications to blastXML-parser.py, ISSUE in line 26 the two for statements should be at same level

--- biopython-scripts/blastXML-parser.py
+++ biopython-scripts/blastXML-parser.py
@@ -22,9 +22,13 @@ with open(inputfileName, 'r') as result_handle:
 	blast_records = NCBIXML.parse(result_handle)
 	for blast_record in blast_records:
 		print '\n\n%s' % blast_record.query #prints query's full name
+		print 'Query lenght: %d' % blast_record.query_length
 		for description in blast_record.descriptions:
-			print 'Hit name = %s' % description.title
-			print 'evalue = %s' % str(description.e) # loops on all hits for the query and prints the corresponding e-value
+			for alignment in blast_record.alignments:
+				print 'Hit name = %s' % description.title
+				print 'evalue = %s' % str(description.e) # loops on all hits for the query and prints the corresponding e-value
+				print 'Hit length = %s' % str(alignment.length)
+				print 'Number of Alignments = %s' % str(description.num_alignments)
 			#e_value_dict[blast_record.query] = description.e
 			#print "%s : %s" % (blast_record.query, str(description.e))
 
