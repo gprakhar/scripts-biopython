90bfc2a523869f94c909c97f0df1fc7dae2f8070
gprakhar <prakhar.aaidu16@gmail.com>
2015-07-27 14:18:14 +0530

    pruned out unnecessary code, moved usable code to present location

--- passwd-generator.sh
+++
@@ -1,7 +0,0 @@
-#Use Kernel's urandom device to generate password
-#do NOT use on a multiuser system (multiple people using it)
-
-l=$1
-	[ "$l" == "" ] && l=16
-      	tr -dc A-Za-z0-9_ < /dev/urandom | head -c ${l} | xargs
- 
