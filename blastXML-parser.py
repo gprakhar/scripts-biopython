8f0fb6cc4e5e88322f8c7710c5526f69454da0e0
gprakhar <prakhar.aaidu16@gmail.com>
2015-07-27 14:26:37 +0530

    rolled back blastXML-parser.py to master branch version

--- blastXML-parser.py
+++ blastXML-parser.py
@@ -4,33 +4,28 @@
 
 from Bio.Blast import NCBIXML
 import os, glob
-
+'''
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('Xmlfilename', metavar='f', help='name of xml file')
+parser.add_argument('lenghtofAminoAcidSe', metavar='N', help='lenght of the amino acid sequence', type=int)
+parser.add_argument('-v',
+'''
 
-args = parser.parse_args()
-inputfileName = str(args.Xmlfilename)
-
-#filenames = [os.path.basename(x) for x in glob.glob('*.xml')]
+filenames = [os.path.basename(x) for x in glob.glob('*.xml')]
 
 e_value_dict = {}
 
-with open(inputfileName, 'r') as result_handle:
-	#read xml output file
-	blast_records = NCBIXML.parse(result_handle)
-	for blast_record in blast_records:
-		print '\n\n%s' % blast_record.query #prints query's full name
-		print 'Query lenght: %d' % blast_record.query_length
-		for description in blast_record.descriptions:
-			for alignment in blast_record.alignments:
-				print 'Hit name = %s' % description.title
-				print 'evalue = %s' % str(description.e) # loops on all hits for the query and prints the corresponding e-value
-				print 'Hit length = %s' % str(alignment.length)
-				print 'Number of Alignments = %s' % str(description.num_alignments)
-			#e_value_dict[blast_record.query] = description.e
-			#print "%s : %s" % (blast_record.query, str(description.e))
+for name in filenames:
+	with open(name, 'r') as result_handle:
+		#read xml output file
+		blast_records = NCBIXML.parse(result_handle)
+		for blast_record in blast_records:
+			#print (blast_record.query) #prints query's full name
+			for description in blast_record.descriptions:
+				#print description.e # loops on all hits for the query and prints the corresponding e-value
+				#e_value_dict[blast_record.query] = description.e
+				print "%s : %s" % (blast_record.query, str(description.e))
 
 '''			
 with open("out1", "w") as out1, open("out2", "w") as out2:
