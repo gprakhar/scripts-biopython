7189b088de6737ae036c1112012cbe72e740dda4
gprakhar <prakhar.aaidu16@gmail.com>
2015-07-28 23:38:07 +0530

    hmmer-parser.py can now parse HMMER3 plain text output, But only file with one hit

--- hmmer-parser.py
+++ hmmer-parser.py
@@ -2,4 +2,24 @@
 #Date: 28 Jul 2015 IST
 #Author: Prakhar Gaur
 
+from Bio import SearchIO
+import os, glob
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument('HMMOutputFilename', metavar='f', help='name of hmmer3 plain text outputfile')
+
+args = parser.parse_args()
+inputfileName = str(args.HMMOutputFilename)
+
+#filenames = [os.path.basename(x) for x in glob.glob('*.xml')]
+for qresult in SearchIO.parse(inputfileName,'hmmer3-text'):
+	for hit in qresult:
+		hits = qresult.hits
+		beste = hits[0].hsps[0].evalue
+        	query = hits[0].query_id
+        	hit = hits[0].id.replace('.hmm', '')
+        	print query + ',' + hit + ',' + str(beste)
+		print len(hit)
+	
+	
