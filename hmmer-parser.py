499ecbd4f8d6781346d387f5373cf9a20237e74e
gprakhar <prakhar.aaidu16@gmail.com>
2015-07-29 14:03:19 +0530

    hmmer-parser.py output contains, protName-numHits-queryName-numHSP-NameHit-HSPbitscore, reformated output

--- hmmer-parser.py
+++ hmmer-parser.py
@@ -17,7 +17,7 @@ filenames = [os.path.basename(x) for x in glob.glob('*.out')]
 for filename in filenames:
 	print '\nProtien Name = %s' % filename[:-4]
 	for qresult in SearchIO.parse(filename,'hmmer3-text'):
-		print 'lenght of qresult = %d' % len(qresult)
+		print '\n\nNumber of Hits = %d' % len(qresult)
 		print 'Query Id = %s' % (qresult.id)
 		flag = 0
 		for hit in qresult:
@@ -28,22 +28,15 @@ for filename in filenames:
 				print '.'
 				break
 			else:
-				#print '\nQuery Id = %s' % (qresult.id)
-				print '%d. number of HSP = %d' % (flag, len(hit))
-				#print hit
-#				beste = hit[0].hsps[0].evalue
-	       			Hit = hit.id
-	  #      		hit = hit[0].id.replace('.hmm', '')
-#		        	print query + ',' + hit + ',' + str(beste)
-				print 'Name of Hit =  %s' % Hit
+				print '\nName of Query = %s' % (qresult.id)
+				print '\n%d.Name of Hit =  %s' % (flag, hit.id)
+				print 'number of HSP = %d' % (len(hit))
 #				print 'Discription of Query %s' % hit.query_description
 				flag2 = 0
 			for hsp in hit:
 				flag2 = flag2 +1
 				if flag2 > 3:
 					print '.'
-	                                print '.'
-        	                        print '.'
                 	                break
 				else:
 					print 'HSP bitscore : %s' % (str(hsp.bitscore))
