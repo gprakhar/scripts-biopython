2bed3a97fbf9f0f7eb66f24351253f1de06c4a6d
gprakhar <prakhar.aaidu16@gmail.com>
2015-07-29 14:37:07 +0530

    hmmer-parser.py output contains, protName-numHits-queryName-numHSP-NameHit-HSPbitscore, Input mode - file name at command line, No glob

--- hmmer-parser.py
+++ hmmer-parser.py
@@ -6,38 +6,38 @@ from Bio import SearchIO
 import os, glob
 import argparse
 
-#parser = argparse.ArgumentParser()
-#parser.add_argument('HMMOutputFilename', metavar='f', help='name of hmmer3 plain text outputfile')
+parser = argparse.ArgumentParser()
+parser.add_argument('HMMOutputFilename', metavar='f', help='name of hmmer3 plain text outputfile')
 
-#args = parser.parse_args()
-#inputfileName = str(args.HMMOutputFilename)
+args = parser.parse_args()
+inputfileName = str(args.HMMOutputFilename)
 
-filenames = [os.path.basename(x) for x in glob.glob('*.out')]
+#filenames = [os.path.basename(x) for x in glob.glob('*.out')]
 
-for filename in filenames:
-	print '\nProtien Name = %s' % filename[:-4]
-	for qresult in SearchIO.parse(filename,'hmmer3-text'):
-		print '\n\nNumber of Hits = %d' % len(qresult)
-		print 'Query Id = %s' % (qresult.id)
-		flag = 0
-		for hit in qresult:
-			flag = flag + 1
-			if flag > 5:
+#for filename in filenames:
+#	print '\nProtien Name = %s' % filename[:-4]
+print '\nProtien Name = %s' % inputfileName[:-4]
+for qresult in SearchIO.parse(inputfileName,'hmmer3-text'):
+	print 'Query Name = %s' % (qresult.id)
+	print '\n\nNumber of Hits = %d' % len(qresult)
+	flag = 0
+	for hit in qresult:
+		flag = flag + 1
+		if flag > 5:
+			print '.'
+			print '.'
+			print '.'
+			break
+		else:
+			print '\n%d.Name of Hit =  %s' % (flag, hit.id)
+			print 'number of HSP = %d' % (len(hit))
+#			print 'Discription of Query %s' % hit.query_description
+			flag2 = 0
+		for hsp in hit:
+			flag2 = flag2 +1
+			if flag2 > 3:
 				print '.'
-				print '.'
-				print '.'
-				break
+               	                break
 			else:
-				print '\nName of Query = %s' % (qresult.id)
-				print '\n%d.Name of Hit =  %s' % (flag, hit.id)
-				print 'number of HSP = %d' % (len(hit))
-#				print 'Discription of Query %s' % hit.query_description
-				flag2 = 0
-			for hsp in hit:
-				flag2 = flag2 +1
-				if flag2 > 3:
-					print '.'
-                	                break
-				else:
-					print 'HSP bitscore : %s' % (str(hsp.bitscore))
+				print 'HSP bitscore : %s' % (str(hsp.bitscore))
 	print '*' * 100
