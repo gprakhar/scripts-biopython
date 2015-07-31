9977d744ad00acac58694fa8d2e88e452e8cf437
gprakhar <prakhar.aaidu16@gmail.com>
2015-08-01 03:44:27 +0530

    blast-SearchIO-XML-parser.py, working producing output simelar to hmmer parser SearchIO code

--- blast-SearchIO-XML-parser.py
+++ blast-SearchIO-XML-parser.py
@@ -1,5 +1,5 @@
-#Script to parse Plain text output from Blast+2.30
-#Date: 28 Jul 2015 IST
+#Script to parse Plain text output from Blast+2.30 blastp on Dicty protien set
+#Date: 01 Aug 2015 IST
 #Author: Prakhar Gaur
 
 from Bio import SearchIO
@@ -13,23 +13,10 @@ import os, glob
 #inputfileName = str(args.HMMOutputFilename)
 
 filenames = [os.path.basename(x) for x in glob.glob('*.xml')]
-flag_once = 0
-print filenames
-filenames.sort()
-print filenames
 
 for filename in filenames:
-	'''protName_new = filename[8:14]
-	if flag_once == 0:
-		pass
-	else:
-		if protName != protName_new:
-			print '---XXXX---' * 10
-	flag_once = flag_once + 1
-	protName = filename[8:14]'''
-	print '\nProtien Name = %s' % filename[8:14]
-	qresults = SearchIO.parse(filename,'hmmer3-text')
-	'''for qresult in qresults:
+	qresults = SearchIO.parse(filename,'blast-xml')
+	for qresult in qresults:
 		print 'Query Name = %s' % (qresult.id)
 		print 'Query Details = %s' % (qresult.description)
 		print '\n\nNumber of Hits = %d' % len(qresult)
@@ -42,9 +29,9 @@ for filename in filenames:
 				print '.'
 				break
 			else:
-				print '\n%d.Name of Hit =  %s' % (flag, hit.id)
+				print '\n%d. ID of Hit =  %s' % (flag, hit.id)
+				print 'Name of Hit = %s' % (hit.description)
 				print 'number of HSP = %d' % (len(hit))
-#				print 'Discription of Query %s' % hit.query_description
 				flag2 = 0
 			for hsp in hit:
 				flag2 = flag2 +1
@@ -53,4 +40,5 @@ for filename in filenames:
 	               	                break
 				else:
 					print 'HSP bitscore : %s' % (str(hsp.bitscore))
-		print '##' * 10'''
+					print 'evalue of HSP = %s' % (str(hsp.evalue))
+		print '##' * 10
