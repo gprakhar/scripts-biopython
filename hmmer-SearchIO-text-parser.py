2860cf5378923ac900f5846fa21f9c165a880f2c
gprakhar <prakhar.aaidu16@gmail.com>
2015-08-17 13:53:04 +0530

    both blast & hmmer SearchIO based, search scripts producing identical output, result formatting improved

--- hmmer-SearchIO-text-parser.py
+++ hmmer-SearchIO-text-parser.py
@@ -1,4 +1,4 @@
-#Script to parse Plain text output from HMMER 3.1b2
+#Script to parse Plain text output from HMMER 3.1b2, using the Biopython SerachIO library
 #Date: 28 Jul 2015 IST
 #Author: Prakhar Gaur
 
@@ -12,43 +12,36 @@ import os, glob
 #args = parser.parse_args()
 #inputfileName = str(args.HMMOutputFilename)
 
-filenames = [os.path.basename(x) for x in glob.glob('*.out')]
+filenames = [os.path.basename(x) for x in glob.glob('*.OUT')]
 flag_once = 0
 
 for filename in filenames:
-	'''protName_new = filename[8:14]
-	if flag_once == 0:
-		pass
-	else:
-		if protName != protName_new:
-			print '---XXXX---' * 10
-	flag_once = flag_once + 1
-	protName = filename[8:14]'''
-	print '\nProtien Name = %s' % filename[8:14]
+	print '\nProtien Name = %s' % filename[:-4]
 	qresults = SearchIO.parse(filename,'hmmer3-text')
 	for qresult in qresults:
-		print 'Query Name = %s' % (qresult.id)
-		print 'Query Details = %s' % (qresult.description)
-		print '\n\nNumber of Hits = %d' % len(qresult)
+		print '\n\tDomain Query Name = %s' % (qresult.id)
+		print '\tDomain Details = %s' % (qresult.description)
+		print '\tNumber of Hits = %d' % len(qresult)
 		flag = 0
 		for hit in qresult:
 			flag = flag + 1
 			if flag > 5:
-				print '.'
-				print '.'
-				print '.'
+				print '\t\t..'
+				print '\t\t..'
 				break
 			else:
-				print '\n%d.Name of Hit =  %s' % (flag, hit.id)
-				print 'number of HSP = %d' % (len(hit))
-				print 'Discription of Query %s' % hit.description
+				print '\n\t\t%d.Name of Hit =  %s' % (flag, hit.id)
+				print '\t\tDescription of Hit = %s' % (hit.description)
+				print '\t\tnumber of HSP = %d' % (len(hit))
 				flag2 = 0
 			for hsp in hit:
 				flag2 = flag2 +1
 				if flag2 > 3:
-					print '.'
+					print '\t\t\t...'
 	               	                break
 				else:
-					print 'HSP bitscore : %s' % (str(hsp.bitscore))
-					print 'evalue of HSP : %s' % (str(hsp.evalue))
-		print '##' * 10
+					print '\t\t\tHSP bitscore : %s' % (str(hsp.bitscore))
+					print '\t\t\tevalue of HSP : %s' % (str(hsp.evalue))
+	print '##' * 10
+
+
